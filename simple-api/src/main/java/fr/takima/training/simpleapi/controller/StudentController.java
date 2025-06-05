b72e3e6e6642d45f109f9d1d0f4fa26fec987d61
Aymen Kadri <82622888+aymendev8@users.noreply.github.com>
2025-06-05 13:55:07 +0200

    Update Docker container configuration and change student API endpoint

--- simple-api/src/main/java/fr/takima/training/simpleapi/controller/StudentController.java
+++ simple-api/src/main/java/fr/takima/training/simpleapi/controller/StudentController.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 
 @RestController
 @CrossOrigin
-@RequestMapping(value = "/students")
+@RequestMapping(value = "/student")
 public class StudentController {
     private final StudentService studentService;
 
